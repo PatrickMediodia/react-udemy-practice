e80df5621b1bd0a87dfbd27ee880a1141e61826d
Pat <mediodia.patrick@gmail.com>
2024-02-17 00:21:32 +0800

    first component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,14 +1,23 @@
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+// components must be
+// 1. a function with an uppercase letter
+// 2. must return something that can be rendered
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
