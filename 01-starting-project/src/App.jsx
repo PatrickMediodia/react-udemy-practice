8abb15f2683baa0cc58e4f180125776507ace75c
Patrick Mediodia <mediodia.patrick@gmail.com>
2024-02-17 08:09:35 +0800

    added props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,25 +1,56 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data'; // named export and not default
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length-1)];
+  
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {description} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
 }
 
-// components must be
-// 1. a function with an uppercase letter
-// 2. must return something that can be rendered
+// react sends the data as the object "props"
+// in this case we destructured it
+function CoreConcept({ title, description, image }) {
+  return(
+    <li>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <ul>
+            {
+              CORE_CONCEPTS.map(({title, description, image}) => {
+                return <CoreConcept 
+                title={title} 
+                description={description}
+                image={image}
+                />
+              })
+            }
+          </ul>
+        </section>
       </main>
     </div>
   );
