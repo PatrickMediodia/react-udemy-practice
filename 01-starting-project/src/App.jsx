e79f30a0a0346a98847a8b056e8d49f0108100c0
Patrick Mediodia <mediodia.patrick@gmail.com>
2024-02-17 10:15:14 +0800

    conditional rendering

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,21 +1,44 @@
+// react hooks
+import { useState } from 'react';
+
 // named export and not default
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 
 // default exports
 import CoreConcept  from './components/CoreConcept/CoreConcept';
 import Header from './components/Header/Header';
 import TabButton from './components/TabButton/TabButton';
 
-// react hooks
-import { useState } from 'react';
-
 function App() {
-  const [ displayString, setDisplayString ] = useState('');
+  // 1. hooks can only be called inside of component funcitons
+  // 2. hooks can only be called on the top level
+  // allows the management of component specific state
+  // always returns an array with exactly two items
+  //    [0] --> current state
+  //    [1] --> function to update the state
+  // note if state changes, it will re rendert the whole component
+  const [ displayString, setDisplayString ] = useState();
+  const tabs = ['components', 'jsx', 'props', 'state']
 
   const handleClick = (dynamicContent) => {
-    console.log(dynamicContent);
     setDisplayString(dynamicContent);
+
+    // react schedules the update of the state afte it has been re-rendered
+    // therefore if you log the dynamic content now, it will still show the old state
+    console.log(dynamicContent);
   };
+
+  const tabContent = (
+    <div id="tab-content">
+      <h3>{ EXAMPLES[displayString].title }</h3>
+      <p>{ EXAMPLES[displayString].description }</p>
+      <pre>
+        <code>
+          { EXAMPLES[displayString].code }
+        </code>
+      </pre>           
+    </div>
+  );
 
   return (
     <div>
@@ -37,14 +60,15 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            { /* will not be executed immediately since it is inside an arrow function */ }
-            { /* therefore we can pass parameters to the function */ }
-            <TabButton onClickHandler={() => handleClick('Components') }>Components</TabButton>
-            <TabButton onClickHandler={() => handleClick('JSX')}>JSX</TabButton>
-            <TabButton onClickHandler={() => handleClick('Props')}>Props</TabButton>
-            <TabButton onClickHandler={() => handleClick('State')}>State</TabButton>
+            {
+              /* will not be executed immediately since it is inside an arrow function */
+              /* therefore we can pass parameters to the function */
+              tabs.map((tab) => {
+                return <TabButton onClickHandler={() => handleClick(tab) }>{tab}</TabButton>
+              })
+            }
           </menu>
-          { displayString }
+          { !displayString ?  <p>Please select a topic.</p> : tabContent }
         </section>
       </main>
     </div>
