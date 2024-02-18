7c764a44e1a3491c4dbeaf93ca3ec7e6493170d6
Pat <mediodia.patrick@gmail.com>
2024-02-18 16:10:28 +0800

    migrate all to tailwind

--- 04-styling-react-components/src/components/AuthInputs.jsx
+++ 04-styling-react-components/src/components/AuthInputs.jsx
@@ -2,18 +2,18 @@ import { useState } from 'react';
 import { styled } from 'styled-components';
 
 import Button from './Button.jsx';
-import CustomInput from './Input.jsx';
+import Input from './Input.jsx';
 
 // tagged templates, regular javascript not react specific
 // standard css
 // returns a react component that returns a div that has these styles applied
 // creates unique class names
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
+// const ControlContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `
 
 // forwards all the props, and then just adds these labels
 // building small element wrappers that can be reused
@@ -40,32 +40,29 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
         <p>
-          { /* so that it wont clash with */ }
-          <CustomInput
+          <Input
             label="Email"
             invalid={emailNotValid}
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            //className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <CustomInput
+          <Input
             label="Password"
             type="password"
-            $invalid={passwordNotValid}
-            className={passwordNotValid ? 'invalid' : undefined}
-            onChange={(event) =>
-            handleInputChange('password', event.target.value)
-            }
+            invalid={passwordNotValid}
+            //className={passwordNotValid ? 'invalid' : undefined}
+            onChange={(event) => handleInputChange('password', event.target.value)}
           />
         </p>
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
